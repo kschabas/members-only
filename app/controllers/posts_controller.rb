37dd978a9eb1e2a0940851ad2570c8da5d98118c
Karl Schabas <karl.schabas@utoronto.ca>
2024-05-21 16:09:40 -0400

    Preliminary authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,32 @@
 class PostsController < ApplicationController
 
-def new
-  @post = Post.new
-end
+  before_action :authenticate_user!, only: [:create, :new]
 
-def create
-  @post = Post.new(post_params)
-
-  if @post.save
-    redirect_to @post
-  else
-    render :new, status: :unprocessable_entity
+  def new
+    @post = Post.new
   end
-end
 
-def show
-  @post = Post.find(params[:id])
-end
+  def create
+    @post = Post.new(post_params)
 
-def index
-  @posts = Post.all
-end
-
-private
-  def post_params
-    params.require(:post).permit(:title, :body, :user_id)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
 
 end
